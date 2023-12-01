4812a8b2f20e0746b5ddd30ac5445cdf5c86c44d
abukawsar45 <kawsar1146236@gmail.com>
2023-12-01 10:06:22 +0600

    setup metarial ui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from '@mui/material'
+// import Button from '@mui/material/Button';
+
+function App() {
+  
+  return (
+    <>
+      <Button variant="contained">Hello world</Button> 
+    </>
+  )
+}
+
+export default App
